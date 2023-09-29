fa253e70e466a2fb3d1f88f56aee7e6e54ef340c
Habip Yeşilyurt <yesilyurthabip@hotmail.com>
2023-09-29 16:20:01 +0300

    Initial Commit

---
+++ Appetizers/AppetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersApp.swift
+//  Appetizers
+//
+//  Created by Habip Yesilyurt on 29.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct AppetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
