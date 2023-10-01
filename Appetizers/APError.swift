522e25669d9a1d18af9c6d49cf5b0c0522fe0b8a
Habip Yeşilyurt <yesilyurthabip@hotmail.com>
2023-10-01 08:25:06 +0300

    Create NetworkManager

---
+++ Appetizers/APError.swift
@@ -0,0 +1,15 @@
+//
+//  APError.swift
+//  Appetizers
+//
+//  Created by Habip Yesilyurt on 1.10.2023.
+//
+
+import Foundation
+
+enum APError: Error {
+    case invalidURL
+    case invalidResponse
+    case invalidData
+    case unableToComplete
+}
---
+++ Appetizers/AppetizerListViewModel.swift
@@ -0,0 +1,25 @@
+//
+//  AppetizerListViewModel.swift
+//  Appetizers
+//
+//  Created by Habip Yesilyurt on 1.10.2023.
+//
+
+import SwiftUI
+
+final class AppetizerListViewModel: ObservableObject {
+    @Published var appetizers: [Appetizer] = []
+    
+    func getAppetizers() {
+        NetworkManager.shared.getAppetizers { result in
+            DispatchQueue.main.async {
+                switch result {
+                case .success(let appetizers):
+                    self.appetizers = appetizers
+                case .failure(let error):
+                    print(error.localizedDescription)
+                }
+            }
+        }
+    }
+}
